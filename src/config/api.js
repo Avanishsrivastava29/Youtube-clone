5343782bf03dc43cf4cf669aa1f0c8e9bd3ab21b
Avanish srivastava <avanishsrivastava78@gmail.com>
2023-12-08 17:36:21 +0530

    api updated

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
 const API_KEY = "AIzaSyCVw0IVlAQUxxpzCvLG2CDh79ElWEMMHl4"
 
- export const YOUTUBE_MOST_POPULAR_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+API_KEY;
+ export const YOUTUBE_MOST_POPULAR_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key="+API_KEY;
